f9d02bb8aa1f416edd2a1a3bb48ff3718a62b243
van jordan <vanjordan46@hotmail.com>
2019-07-12 09:03:18 -0700

    MVP

--- api/actionRouter.js
+++ api/actionRouter.js
@@ -64,7 +64,7 @@ router.put('/:id', async (req, res) => {
 //
 router.post('/', async (req, res) => {
     try{
-        const action = await Actions.insert(res.body);
+        const action = await Actions.insert(req.body);
         res.status(200).json(action);
     }catch(err){
         console.log(err);
